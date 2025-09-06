16b542bf44e0fd6acf22686bd235bab7f2780cd2
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-09-06 11:08:37 +0100

    modif

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
+
 export const nextServer = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL,
   withCredentials: true,
 });
